737561a50bb9ce9b96796db4ccf5a67ed68ac205
Allan Jardine <allan@datatables.net>
2026-03-17 09:53:52 +0000

    Tests: Additional checks on different files

--- test/cdn-compare.sh
+++ test/cdn-compare.sh
@@ -20,14 +20,14 @@ passed=0
 
 # Compare function
 compare() {
-	FILE=$1
+	FILE="$1"
 
 	curl -o $TMPFILE_CDN --silent "${CDN_ADDRESS}/${FILE}"
 	curl -o $TMPFILE_LOCAL --silent "${LOCAL_ADDRESS}/${FILE}"
 
 	if cmp -s $TMPFILE_CDN $TMPFILE_LOCAL; then
 		echo "Pass: $FILE"
-		passed=$((failed+1))
+		passed=$((passed+1))
 	else
 		echo "FAIL: $FILE"
 		failed=$((failed+1))
@@ -35,6 +35,16 @@ compare() {
 
 	rm $TMPFILE_CDN
 	rm $TMPFILE_LOCAL
+}
+
+# Run compare on the standard set of files
+compare_files() {
+	MODULES="$1"
+
+	compare "${MODULES}/datatables.js"
+	compare "${MODULES}/datatables.css"
+	compare "${MODULES}/datatables.min.js"
+	compare "${MODULES}/datatables.min.css"
 }
 
 # Check that the cache is available
@@ -55,30 +65,18 @@ PID=$(jobs -p)
 echo "Server running"
 
 # Run tests
-compare "dt/dt-2.0.0/datatables.js"
-compare "dt/dt-2.0.0/datatables.css"
-compare "dt/dt-2.0.0/datatables.min.js"
-compare "dt/dt-2.0.0/datatables.min.css"
-
-compare "dt/dt-2.3.7/datatables.js"
-compare "dt/dt-2.3.7/datatables.css"
-compare "dt/dt-2.3.7/datatables.min.js"
-compare "dt/dt-2.3.7/datatables.min.css"
-
-compare "dt/dt-2.3.7/b-3.2.6/b-html5-3.2.6/datatables.js"
-compare "dt/dt-2.3.7/b-3.2.6/b-html5-3.2.6/datatables.css"
-compare "dt/dt-2.3.7/b-3.2.6/b-html5-3.2.6/datatables.min.js"
-compare "dt/dt-2.3.7/b-3.2.6/b-html5-3.2.6/datatables.min.css"
-
-compare "bs4-4.6.0/jq-3.7.0/dt-2.3.7/cc-1.2.1/datatables.css"
-compare "bs4-4.6.0/jq-3.7.0/dt-2.3.7/cc-1.2.1/datatables.js"
-compare "bs4-4.6.0/jq-3.7.0/dt-2.3.7/cc-1.2.1/datatables.min.css"
-compare "bs4-4.6.0/jq-3.7.0/dt-2.3.7/cc-1.2.1/datatables.min.js"
-
-compare "bs4/jq-3.7.0/dt-2.3.7/cc-1.2.1/datatables.css"
-compare "bs4/jq-3.7.0/dt-2.3.7/cc-1.2.1/datatables.js"
-compare "bs4/jq-3.7.0/dt-2.3.7/cc-1.2.1/datatables.min.css"
-compare "bs4/jq-3.7.0/dt-2.3.7/cc-1.2.1/datatables.min.js"
+compare_files "dt/dt-2.0.0"
+compare_files "dt/dt-2.3.7"
+compare_files "dt/dt-2.3.7/b-3.2.6/b-html5-3.2.6"
+compare_files "bs4-4.6.0/jq-3.7.0/dt-2.3.7/cc-1.2.1"
+compare_files "bs4/jq-3.7.0/dt-2.3.7/cc-1.2.1"
+compare_files "bm/jq-3.7.0/dt-2.3.7/cr-2.1.2/sc-2.4.3"
+compare_files "bs5/dt-2.2.0/date-1.5.0"
+compare_files "dt/dt-1.13.11"
+compare_files "dt/dt-1.13.11/b-2.4.2"
+compare_files "bs5/jq-3.7.0/dt-2.3.7/af-2.7.1/b-3.2.6/b-colvis-3.2.6/b-html5-3.2.6/cr-2.1.2/cc-1.2.1/date-1.6.3/fc-5.0.5/fh-4.0.6/kt-2.12.2/r-3.0.8/rg-1.6.0/rr-1.5.1/sc-2.4.3/sb-1.8.4/sp-2.3.5/sl-3.1.3/sr-1.4.3"
+compare_files "dt/dt-2.1.0/cc-1.0.0"
+compare_files "se/dt-2.3.7/cc-1.2.1/sl-3.1.3"
 
 # Stop the server
 kill $PID
